6899f3379e833e33211028c42ceaa1127a29a363
aimrane <aimrane.essakhi@gmail.com>
2023-08-18 16:21:23 +0100

    upload file pdf v2

--- src/interfaces.d.ts
+++ src/interfaces.d.ts
@@ -1,27 +1,23 @@
-import { AxiosHeaders } from "axios"
-import {
-  HeaderDescription,
-  HeaderTitle,
-  ResponseProperties
-} from "./enums"
+import { AxiosHeaders } from "axios";
+import { HeaderDescription, HeaderTitle, ResponseProperties } from "./enums";
 
 export interface ChatGPTMessageProps {
-  role: string
-  content: string
+  role: string;
+  content: string;
 }
 
 export interface CorrectionProps {
-  id: string
-  type: string
-  explanation: string
-  result: string[]
+  id: string;
+  type: string;
+  explanation: string;
+  result: string[];
 }
 
 export interface CorrectionsListProps extends Array<CorrectionProps> {}
 
 export interface CorrectionsProps {
-  correctionsList: CorrectionsListProps
-  correctedText: string
+  correctionsList: CorrectionsListProps;
+  correctedText: string;
 }
 
 export interface MakeRequestProps {
@@ -29,52 +25,64 @@ export interface MakeRequestProps {
     url: string,
     payload: ChatGPTJSON_dataProps,
     headers: AxiosHeaders["Content-Type, Authorization"]
-  ): Promise<any>
+  ): Promise<any>;
 }
 
 export interface ChatGPTJSON_dataProps {
-  model: string
-  messages: ChatGPTMessageProps[]
-  temperature: number
+  model: string;
+  messages: ChatGPTMessageProps[];
+  temperature: number;
 }
 
 export interface NavbarLinkProps {
-  name: string
-  href: string
-  icon: string
-  backgroundColor: string
+  name: string;
+  href: string;
+  icon: string;
+  backgroundColor: string;
 }
 
 export interface BannerProps {
-  title: HeaderTitle
-  description: HeaderDescription
-  className?: string
+  title: HeaderTitle;
+  description: HeaderDescription;
+  className?: string;
 }
 
 export interface ChildrenProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
-export type AddCorrectionProps = (newCorrections: CorrectionsProps) => void
+export type AddCorrectionProps = (newCorrections: CorrectionsProps) => void;
 
-export type SetSummaryProps = (newSummary: string) => void
-
+export type SetSummaryProps = (newSummary: string) => void;
 
 export interface UseChatGPTProps {
-  initialPrompt: ChatGPTMessageProps
-  customProperty: ResponseProperties
-  customSetState: AddCorrectionProps | SetSummaryProps
+  initialPrompt: ChatGPTMessageProps;
+  customProperty: ResponseProperties;
+  customSetState: AddCorrectionProps | SetSummaryProps;
 }
 
-export type FromText = string
+export type FromText = string;
 
 export interface ModalContentProps {
   content: {
-    "How It Works": string[]
-    Limitations: string[]
-    "Common Mistakes"?: string[]
-    "Common Features"?: string[]
-    Tips: string[]
-    Disclaimers: string[]
-  }
+    "How It Works": string[];
+    Limitations: string[];
+    "Common Mistakes"?: string[];
+    "Common Features"?: string[];
+    Tips: string[];
+    Disclaimers: string[];
+  };
+}
+
+interface UploadedFile extends File {
+  isUploading: boolean;
+}
+
+interface FileUploadProps {
+  setFile: Dispatch<SetStateAction<File | null>>;
+  setRes: Dispatch<SetStateAction<any>>;
+}
+
+interface FileItemProps {
+  file: File | null;
 }
