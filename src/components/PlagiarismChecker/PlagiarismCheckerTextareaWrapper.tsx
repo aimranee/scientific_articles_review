013f94b842a3576a06d417bac8cffa52ac6de307
maryemelamri <meriammeri.mm@gmail.com>
2023-08-23 17:58:52 +0100

    plagia get data in front

--- src/components/PlagiarismChecker/PlagiarismCheckerTextareaWrapper.tsx
+++ src/components/PlagiarismChecker/PlagiarismCheckerTextareaWrapper.tsx
@@ -1,16 +1,17 @@
 import { usePlagiarismChecker } from "@/hooks/usePlagiarismChecker"
 import PlagiarismCheckerTextareaFooter from "./PlagiarismCheckerTextareaFooter"
 import PlagiarismCheckerTextarea from "./PlagiarismCheckerTextarea"
-import text_to_check from "@/services/askToTurnitin"
+// import {text_to_check} from "@/services/askToTurnitin"
+
 
 const PlagiarismCheckerTextareaWrapper = (): JSX.Element => {
   const { onChange, loading, setTextToCorrect } = usePlagiarismChecker()
-
+// const {text_to_check} = per
   return (
     <div className="border dark:border-gray-2 bg-white dark:bg-gray-1 rounded-md shadow-lg">
       {/* <PlagiarismCheckerTextarea onChange={onChange} setTextToCorrect={setTextToCorrect} />
       <PlagiarismCheckerTextareaFooter loading={loading} /> */}
-      {text_to_check}
+      {/* {text_to_check} */}
 
     </div>
   )
