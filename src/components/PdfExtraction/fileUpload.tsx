e3e83dacdba2984831553dc003dbb4d7131fa246
aimrane <aimrane.essakhi@gmail.com>
2023-09-11 15:03:20 +0100

    update upload v7

--- src/components/PdfExtraction/fileUpload.tsx
+++ src/components/PdfExtraction/fileUpload.tsx
@@ -8,8 +8,7 @@ import {
 import { Upload, Typography, Card, Skeleton } from "antd";
 import axios from "axios";
 import { type FC } from "react";
-const { Title, Paragraph, Text, Link } = Typography;
-
+const { Title, Paragraph } = Typography;
 import type { UploadFile, UploadProps } from "antd/es/upload/interface";
 
 const FileUpload: FC = () => {
@@ -18,39 +17,135 @@ const FileUpload: FC = () => {
   const [loading, setLoading] = useState(false);
   const [res, setRes] = useState<any>(null);
 
-  const components = res ? extractKeysAndValues(res) : []; //          bzaaaaf dyal les functions heer Mimi's idea
+  const body = res ? extractBody(res) : [];
+  const front = res ? extractFront(res) : [];
+  const mergedArray = [...front, ...body];
 
-  function extractKeysAndValues(obj: any) {
+  const handleSettingClick = (current: any) => {
+    // Handle the click for the Setting icon
+    console.log("Setting icon clicked for:", current);
+  };
+
+  const handleEditClick = (current: any) => {
+    // Handle the click for the Edit icon
+    console.log("Edit icon clicked for:", current);
+  };
+
+  const handleEllipsisClick = (current: any) => {
+    // Handle the click for the Ellipsis icon
+    console.log("Ellipsis icon clicked for:", current);
+  };
+
+  function extractFront(obj: any) {
     const components: JSX.Element[] = [];
 
     function recurse(current: any, property: any) {
-      // if (current === "front")
-      //   if (current === "article-meta") {
-      //     for (let i = 0; i < current.length; i++) {
-      //       recurse(current[i], property + ".");
-      //     }
-      //   }
+      // console.log("LOG1 => current : " + current + "  property : " + property);
       if (Array.isArray(current)) {
-        console.log(
-          "LOG1 => current : " + current + "  property : " + property
-        );
+        for (let i = 0; i < current.length; i++) {
+          recurse(current[i], property);
+        }
+      } else if (typeof current === "object") {
+        for (const key in current) {
+          if (key !== "$" && key !== "body" && key !== "back") {
+            if (current.hasOwnProperty(key)) {
+              if (property === "abstract") {
+                const title = (
+                  <Card style={{ marginTop: 16 }} loading={loading}>
+                    <Skeleton loading={loading} active>
+                      <Typography.Title
+                        editable
+                        level={4}
+                        style={{ margin: 0 }}
+                      >
+                        Abstract
+                      </Typography.Title>
+                    </Skeleton>
+                  </Card>
+                );
+                components.push(title);
+              }
+              recurse(current[key], key);
+            }
+          }
+        }
+      } else {
+        // console.log(
+        //   "LOG1 => current : " + current + "  property : " + property
+        // );
+        if (property === "article-title") {
+          const bigTitle = (
+            <Title style={{ marginTop: 30 }} level={2}>
+              <Skeleton loading={loading} active>
+                "{current}"
+              </Skeleton>
+            </Title>
+          );
+          components.push(bigTitle);
+        }
+
+        if (property === "p") {
+          const p = (
+            <Card
+              style={{ marginTop: 16 }}
+              actions={[
+                <SettingOutlined
+                  key="setting"
+                  onClick={() => handleSettingClick(current)}
+                />,
+                <EditOutlined
+                  key="edit"
+                  onClick={() => handleEditClick(current)}
+                />,
+                <EllipsisOutlined
+                  key="ellipsis"
+                  onClick={() => handleEllipsisClick(current)}
+                />,
+              ]}
+            >
+              <Skeleton loading={loading} active>
+                <Paragraph
+                  editable={{
+                    autoSize: { maxRows: 5, minRows: 3 },
+                  }}
+                >
+                  {current}
+                </Paragraph>
+              </Skeleton>
+            </Card>
+          );
+          components.push(p);
+        }
+      }
+    }
+
+    recurse(obj, "");
+
+    return components;
+  }
+
+  function extractBody(obj: any) {
+    const components: JSX.Element[] = [];
+
+    function recurse(current: any, property: any) {
+      if (Array.isArray(current)) {
+        // console.log(
+        //   "LOG1 => current : " + current + "  property : " + property
+        // );
 
         for (let i = 0; i < current.length; i++) {
           recurse(current[i], property);
         }
       } else if (typeof current === "object") {
         for (const key in current) {
-          if (key !== "xref" && key !== "journal-meta" && key !== "back") {
+          if (
+            key !== "xref" &&
+            key !== "$" &&
+            key !== "front" &&
+            key !== "back"
+          ) {
             if (current.hasOwnProperty(key)) {
-              // if (key.includes("article-meta")) {
-              //   console.log("hhhhhhhhhhhhhhhhhhhhhhhhh");
-
-              //   for (let i = 0; i < current.length; i++) {
-              //     recurse(current[key], property + "." + key);
-              //   }
-              // } else {
               recurse(current[key], key);
-              // }
             }
           }
         }
@@ -66,24 +161,29 @@ const FileUpload: FC = () => {
           );
           components.push(title);
         }
-        if (property === "title-group.article-title") {
-          const bigTitle = <Title level={2}>Guidelines and Resources</Title>;
-          components.push(bigTitle);
-        }
+
         if (property === "_" || property === "p") {
           const p = (
             <Card
               style={{ marginTop: 16 }}
               actions={[
-                <SettingOutlined key="setting" />,
-                <EditOutlined key="edit" />,
-                <EllipsisOutlined key="ellipsis" />,
+                <SettingOutlined
+                  key="setting"
+                  onClick={() => handleSettingClick(current)}
+                />,
+                <EditOutlined
+                  key="edit"
+                  onClick={() => handleEditClick(current)}
+                />,
+                <EllipsisOutlined
+                  key="ellipsis"
+                  onClick={() => handleEllipsisClick(current)}
+                />,
               ]}
             >
               <Skeleton loading={loading}>
                 <Paragraph
                   editable={{
-                    maxLength: 50,
                     autoSize: { maxRows: 5, minRows: 3 },
                   }}
                 >
@@ -111,11 +211,10 @@ const FileUpload: FC = () => {
     },
     beforeUpload: async (file) => {
       setFileList([file]);
+      setLoading(true);
 
       const formData = new FormData();
-
       formData.append("file-upload", file, file.name);
-      setLoading(true);
 
       try {
         const response = await axios.post(
@@ -148,40 +247,23 @@ const FileUpload: FC = () => {
         </p>
       </Dragger>
 
-      {res ? (
-        components.map((component, index) => (
+      {res &&
+        mergedArray.map((component, index) => (
           <Fragment key={index}>{component}</Fragment>
-        ))
-      ) : (
-        <>
-          <Card style={{ marginTop: 16 }} loading={loading}>
-            <Typography.Title editable level={4} style={{ margin: 0 }}>
-              Title
-            </Typography.Title>
-          </Card>
-          <Card
-            style={{ marginTop: 16 }}
-            actions={[
-              <SettingOutlined key="setting" />,
-              <EditOutlined key="edit" />,
-              <EllipsisOutlined key="ellipsis" />,
-            ]}
-          >
-            <Skeleton loading={loading}>
-              <Paragraph
-                editable={{
-                  maxLength: 50,
-                  autoSize: { maxRows: 5, minRows: 3 },
-                }}
-              >
-                Paragraph
-              </Paragraph>
-            </Skeleton>
-          </Card>
-        </>
-      )}
+        ))}
+      <>{loading ? <Skeleton style={{ marginTop: 30 }} active /> : <></>}</>
     </>
   );
 };
 
 export default FileUpload;
+
+// const [componentData, setComponentData] = useState(
+//   Array(mergedArray.length).fill("")
+// );
+
+// const handleComponentChange = (index: any, newValue: any) => {
+//   const newData = [...componentData];
+//   newData[index] = newValue;
+//   setComponentData(newData);
+// };
