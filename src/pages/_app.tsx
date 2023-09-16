da3a24431c7565b3d2431704dae92e6aa33c83b4
maryemelamri <meriammeri.mm@gmail.com>
2023-09-16 18:46:13 +0100

    first summry article

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,48 @@
-import type { AppProps } from "next/app";
-import { ToastContainer } from "react-toastify";
-import { ThemeProvider } from "next-themes";
-import ThemeWrapper from "@/components/ThemeWrapper";
-import HeadWrapper from "@/components/HeadWrapper";
-import "react-toastify/dist/ReactToastify.css";
-import "@/styles/globals.css";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default function App({ Component, pageProps }: AppProps) {
+function App() {
+  const [file, setFile] = useState(null);
+  const [summary, setSummary] = useState("");
+
+  const handleFileChange = (event: any) => {
+    setFile(event.target.files[0]);
+    console.log(file)
+  };
+
+ const handleSummarize = async () => {
+  if (file) {
+    const formData = new FormData();
+    formData.append("file-upload", file);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/summarize-pdf",
+        formData,
+         
+      );
+
+      if (response.data.success) {
+        setSummary(`Summary saved at: ${response.data.summaryFilePath}`);
+      } else {
+        setSummary("Error summarizing PDF.");
+      }
+    } catch (error) {
+      console.error(error);
+      setSummary("Error summarizing PDF.");
+    }
+  }
+};
+
+
   return (
-    <HeadWrapper>
-      <ThemeProvider>
-        <ThemeWrapper>
-          <ToastContainer limit={2} />
-          <Component {...pageProps} />
-        </ThemeWrapper>
-      </ThemeProvider>
-    </HeadWrapper>
+    <div>
+      <h1>PDF Summary App</h1>
+      <input type="file" accept=".pdf" onChange={handleFileChange} />
+      <button onClick={handleSummarize}>Summarize</button>
+      {summary && <p>{summary}</p>}
+    </div>
   );
 }
+
+export default App;
