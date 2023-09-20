b8b9fbc21a39c145647374d987e162ecd04c78bd
maryemelamri <meriammeri.mm@gmail.com>
2023-09-20 16:49:08 +0100

    summary link it to articles

--- src/components/TextSummarizer/TextSummarizerTextareaWrapper.tsx
+++ src/components/TextSummarizer/TextSummarizerTextareaWrapper.tsx
@@ -1,3 +1,4 @@
+import { type FC, type ChangeEvent } from "react";
 import { useBoundStore } from "@/zustand/useBoundStore";
 import TextSummarizerTextareaFooter from "./TextSummarizerTextareaFooter";
 import { useTextSummarizer } from "@/hooks/useTextSummarizer";
@@ -5,19 +6,37 @@ import ExampleTextButton from "../ExampleTextButton";
 import Textarea from "../Textarea";
 import TextareaWrapper from "../TextareaWrapper";
 import isDesktopView from "@/utils/isDesktopView";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 
+interface TextSummarizerTextareaProps {
+  setTextToSummrize: (text: string) => void;
+}
 const EXAMPLE_TEXT =
   "Virtual reality (VR) is a computer-generated environment that simulates a physical presence in a three-dimensional space. By using VR headsets, users can interact with digital environments that feel like real life. VR is used in a variety of fields, such as gaming, education, and training simulations. Its potential applications are continuously expanding as technology advances.";
 
 const TextSummarizerTextareaWrapper = (): JSX.Element => {
   const { handleSubmit, loading, onChange } = useTextSummarizer();
-  const { value } = useBoundStore();
+  const { value, setValue } = useBoundStore();
+  const router = useRouter();
+  const { data } = router.query;
+  const setText = data ? data.toString() : "";
 
+  useEffect(() => {
+    setValue(setText);
+  }, []);
+
+  const handleTextareaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const newTextValue = e.target.value;
+    onChange(e);
+
+    setValue(newTextValue);
+  };
   return (
     <TextareaWrapper>
       <form onSubmit={handleSubmit}>
         <Textarea
-          onChange={onChange}
+          onChange={handleTextareaChange}
           value={value}
           name="textarea"
           autofocus={isDesktopView()}
