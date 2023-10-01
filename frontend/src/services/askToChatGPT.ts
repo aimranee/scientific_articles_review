df2655b01026a3d55fadec237f6523f81fb11611
maryemelamri <meriammeri.mm@gmail.com>
2023-10-01 19:51:17 +0100

    server chnage apikeyOPenAi

--- frontend/src/services/askToChatGPT.ts
+++ frontend/src/services/askToChatGPT.ts
@@ -1,27 +1,32 @@
-import { ChatGPTJSON_dataProps, MakeRequestProps, ChatGPTMessageProps } from "@/interfaces"
-import { NotificationFailure } from "../utils/toastNotifications"
+import {
+  ChatGPTJSON_dataProps,
+  MakeRequestProps,
+  ChatGPTMessageProps,
+} from "@/interfaces";
+import { NotificationFailure } from "../utils/toastNotifications";
 
 const headers = {
   "Content-Type": "application/json",
-  Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`
-}
+  Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+};
 
-const url = "https://api.openai.com/v1/chat/completions"
+const url = "https://api.openai.com/v1/chat/completions";
 
 const askToChatGPT = async (
   currentChatGPTMessages: ChatGPTMessageProps[],
   makeRequest: MakeRequestProps
 ) => {
   try {
+    console.log("apikey" + process.env.NEXT_PUBLIC_OPENAI_API_KEY);
     const json_data: ChatGPTJSON_dataProps = {
       model: "gpt-3.5-turbo",
       messages: [...currentChatGPTMessages],
-      temperature: 0
-    }
-    await makeRequest(url, json_data, headers)
+      temperature: 0,
+    };
+    await makeRequest(url, json_data, headers);
   } catch (error) {
-    return NotificationFailure("Something went wrong, please try again later")
+    return NotificationFailure("Something went wrong, please try again later");
   }
-}
+};
 
-export default askToChatGPT
+export default askToChatGPT;
